51f83ca8f1952174555c52fac3802e640409a777
arjunsen1999 <arjunsen717@gmail.com>
2023-06-08 22:57:21 +0530

    update listing page

--- client/src/components/ProjectListing/SortDrawer.jsx
+++ client/src/components/ProjectListing/SortDrawer.jsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Drawer,
   DrawerBody,
@@ -6,17 +7,26 @@ import {
   DrawerHeader,
   DrawerOverlay,
   Input,
+  Text,
   useDisclosure,
 } from "@chakra-ui/react";
 import React from "react";
+import { useDispatch } from "react-redux";
+import { handleChangeFilter } from "../../redux/project/Project.action";
+import {RxHamburgerMenu} from "react-icons/rx"
 
 export default function SortDrawer() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const dispatch = useDispatch();
   const btnRef = React.useRef();
+  const handleClick = (value) =>{
+    dispatch(handleChangeFilter(value));
+    onClose();
+  }
   return (
     <>
-      <Button ref={btnRef} colorScheme="teal" onClick={onOpen}>
-        Open
+      <Button ref={btnRef} bg="transparent" onClick={onOpen}>
+        <RxHamburgerMenu />
       </Button>
       <Drawer
         isOpen={isOpen}
@@ -28,14 +38,60 @@ export default function SortDrawer() {
         <DrawerOverlay />
         <DrawerContent>
           {/* <DrawerCloseButton /> */}
-          <DrawerHeader>Create your account</DrawerHeader>
+          <DrawerHeader>Sort By</DrawerHeader>
 
           <DrawerBody>
-            <Input placeholder="Type here..." />
-            <Input placeholder="Type here..." />
-            <Input placeholder="Type here..." />
-            <Input placeholder="Type here..." />
-            <Input placeholder="Type here..." />
+            <Box
+              mb={"10px"}
+              _active={{
+                bg: "#ebf5ff",
+              }}
+              p={"10px 4px"}
+              onClick={() => handleClick("reason")}
+              
+            >
+              <Text fontSize={"18px"}>Reason</Text>
+            </Box>
+            <Box
+              mb={"10px"}
+              _active={{
+                bg: "#ebf5ff",
+              }}
+              p={"10px 4px"}
+              onClick={() => handleClick("category")}
+            >
+              <Text fontSize={"18px"}>Category</Text>
+            </Box>
+            <Box
+              mb={"10px"}
+              _active={{
+                bg: "#ebf5ff",
+              }}
+              p={"10px 4px"}
+              onClick={() => handleClick("status")}
+            >
+              <Text fontSize={"18px"}>Status</Text>
+            </Box>
+            <Box
+              mb={"10px"}
+              _active={{
+                bg: "#ebf5ff",
+              }}
+              p={"10px 4px"}
+              onClick={() => handleClick("location")}
+            >
+              <Text fontSize={"18px"}>Location</Text>
+            </Box>
+            <Box
+              mb={"10px"}
+              _active={{
+                bg: "#ebf5ff",
+              }}
+              p={"10px 4px"}
+              onClick={() => handleClick("project_name")}
+            >
+              <Text fontSize={"18px"}>Project Name</Text>
+            </Box>
           </DrawerBody>
         </DrawerContent>
       </Drawer>
